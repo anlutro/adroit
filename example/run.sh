d34f8e0a6e34da9ee984be495a2cac1ef688dcde
Andreas Lutro <anlutro@gmail.com>
2018-09-12 19:28:27 +0200

    switch to unbound, which is present in both distros

--- example/run.sh
+++ example/run.sh
@@ -3,7 +3,7 @@ set -e
 root=$(dirname $(readlink -f $0))
 
 adroit -l debug -d debian:stretch -r $root base
-adroit -l debug -d debian:stretch -r $root fail2ban
+adroit -l debug -d debian:stretch -r $root unbound
 
 adroit -l debug -d centos:7 -r $root base
-adroit -l debug -d centos:7 -r $root fail2ban
+adroit -l debug -d centos:7 -r $root unbound
